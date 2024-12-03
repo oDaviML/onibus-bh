3e900d8c7dbf72dfdf98beae722382405729a63e
Davi Martins Lage <davimartinslage2005@gmail.com>
2024-12-03 16:33:54 -0300

    .

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,26 @@
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
-	component: () => (
-		<>
-			<div className="p-2 flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Home
-				</Link>{" "}
-				<Link to="/about" className="[&.active]:font-bold">
-					About
-				</Link>
-			</div>
-			<hr />
-			<Outlet />
-			<TanStackRouterDevtools />
-		</>
-	),
+  component: () => (
+    <main className="min-h-screen flex flex-col">
+      <nav className="bg-blue-600 text-white p-4 shadow-md">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-xl font-bold">MyApp</h1>
+          <div className="flex gap-4">
+            <Link
+              to="/"
+              className="hover:text-blue-300 transition-colors [&.active]:font-bold"
+            >
+              Home
+            </Link>
+          </div>
+        </div>
+      </nav>
+
+      <section className="flex-grow">
+
+        <Outlet />
+      </section>
+    </main>
+  ),
 });
