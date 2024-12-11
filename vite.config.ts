fdfa37c132995e0a232c8daa8e572f41d57143c6
Davi Martins Lage <davimartinslage2005@gmail.com>
2024-12-11 10:20:59 -0300

    Configurações deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react(), TanStackRouterVite()],
+	base: "/onibus-bh",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
