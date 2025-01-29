2aeac8c3faabb2983c5469f9b59a68b9c6fefbb3
Davi Martins Lage <davimartinslage2005@gmail.com>
2025-01-29 18:53:29 -0300

    fix: Alteração no axios

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-	baseURL: import.meta.env.VITE_USE_LOCAL_BACKEND ? "http://localhost:8080/api/v1/" : import.meta.env.VITE_BACKEND_URL,
+	baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 export default apiClient;
